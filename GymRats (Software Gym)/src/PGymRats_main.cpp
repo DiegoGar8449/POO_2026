66103b3c8a1806356f50ea25941a98b12d9e822b
Diego Garcia <diegogarcia102010@hotmail.com>
2026-06-07 20:32:36 -0600

    Create PGymRats_main.cpp
    v1.0 del main.
    En POO la doctora nos enseñó a declarar los prototipos de funciones utilizadas en el main, a modo de hacer la función main escalable y flexible.
    El código tiene anotaciones que considero útiles para efectos
    de su uso.
    De momento, estas son las funciones principales que el main
    necesita, a mi consideración.

---
+++ GymRats (Software Gym)/src/PGymRats_main.cpp
@@ -0,0 +1,41 @@
+/* Programa "GymRats". main.cpp del proyecto final.
+ * Cabeceras utilizadas: CPersona.h, 
+ * Autores:
+ * - Contreras Galicia Jesús Yizán,
+ * - García Rodríguez Diego.
+ * Secuencia: 3NV31
+ * U. A.: Programación Orientada a Objetos.
+ * Profa.: Dra. Claudia Azorín Vega.
+ */
+#include <iostream>
+#include "CPersona.h"
+using namespace std;
+
+//PROTOTIPOS (declaraciones de funciones dentro del main)
+void limpiar_pantalla(); // limpia pantalla en linux y windows.
+int seleccionar_perfil(); // Jefe, empleado, entrenador, cte.
+void calcular_sueldo(int opcion); //para empleado y entrenador.
+void mostrar_sueldo(CPersona* empleado); 
+void calcular_membresia(int opcion); //para cliente
+void mostrar_TotalMembresia(CPersona* cliente);
+
+/*************** 1. MAIN ***************/
+int main(){
+	cout<<"*************** GymRats ****************";
+	cout<<"\nPrograma para gestionar tu Gym.";
+	
+	int continuar;
+	do{
+		limpiar_pantalla();
+		int opcion = seleccionar_perfil();
+		calcular_sueldo(opcion);
+		calcular_membresia(opcion);
+		
+		cout<<"\n****************************************";
+		cout<<"\n¿Seleccionar otro perfil?";
+		cin>> continuar;
+	} while (continuar==1);
+	
+	cout<<"\n¡Gracias por usar el programa!";
+	return 0;
+}
