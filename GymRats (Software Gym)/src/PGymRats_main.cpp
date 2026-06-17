6c729a72da94cfddbce9da2e1659c2868f8ff071
Diego Garcia <diegogarcia102010@hotmail.com>
2026-06-17 16:39:14 -0600

    Add files via upload
    Para comenzar a simular una base de datos, se me ocurrió manejar archivos de texto con la librería fstream.
    Implementé el método gestionar_cuenta en la clase hija CJefe con 3 opciones: 1) Crear cuenta. 2) Modificar datos. 3) Salir.
    Cuando se crea una cuenta, los datos se registran en un archivo de texto llamado archivo_jefes.txt.
    Los datos son separados por comas, como en las bases de datos comunes que he manejado en MySQL.

--- GymRats (Software Gym)/src/PGymRats_main.cpp
+++ GymRats (Software Gym)/src/PGymRats_main.cpp
@@ -1,13 +1,14 @@
 /* Programa "GymRats". main.cpp del proyecto final.
  * Cabeceras utilizadas: CPersona.h, 
  * Autores:
- * - Contreras Galicia Jesús Yizán,
+ * - Contreras Galicia Jesús Yizán.
  * - García Rodríguez Diego.
  * Secuencia: 3NV31
  * U. A.: Programación Orientada a Objetos.
  * Profa.: Dra. Claudia Azorín Vega.
  */
 #include <iostream>
+#include <fstream> //para manejo de archivos de texto y simular bd.
 #include "CPersona.h"
 using namespace std;
 
@@ -16,27 +17,27 @@ void limpiar_pantalla(); // limpia pantalla en linux y windows.
 
 /*************** 1. MAIN ***************/
 int main(){
-	cout<<"*************** GymRats ****************";
-	cout<<"\nPrograma para gestionar tu Gym.";
+	cout<<"\n*************** GymRats (software para gestionar tu gym) ****************";
 	
 	int continuar;
 	do{
 		limpiar_pantalla();
 		
 		int opcion;
-		cout<<"\n*************** PERFILES GIMNASIO ***************";
+		cout<<"\n\n PERFILES GIMNASIO ***************";
 		cout<<"\n 1) Jefe. 2) Empleado. 3) Entrenador. 4) Cliente.";
 		cout<<"\nIngrese el numero de perfil al que quiera acceder: ";
 		cin>>opcion;
 		switch(opcion){
 			
 			case 1:
-			CPersona jefe; //objeto
+			CJefe jefe; //objeto
 			jefe.solicitar_datos(); //mensaje
+			jefe.gestionar_cuenta();
 			break;
 		} 
 		
-		cout<<"\n¿Seleccionar otro perfil? 1 = Si. 2 = No: ";
+		cout<<"\n¿Seleccionar otro perfil? 1 = Si. 0 = No: ";
 		cin>> continuar;
 	} while (continuar==1);
 	
@@ -53,7 +54,6 @@ void limpiar_pantalla(){
 			system("clear");//comando para linux.
 	#endif		
 }
-
 /*************** 2. FUNCIONES DEL PROGRAMADOR ***************/
 
 //CONSTRUCTOR CPersona
@@ -84,4 +84,53 @@ void CPersona::solicitar_datos(){
 	cout<<"\nColonia: ";
 	std::getline(std::cin >> std::ws, colonia);
 	cout<<"****************************************";
+}
+
+//CONSTRUCTOR CJefe
+CJefe::CJefe() : CPersona(){
+	id = "";
+	contrasenia = "";
+}
+
+//DEFINICION METODOS CJefe
+void CJefe::gestionar_cuenta(){
+	int opcion_cuenta;
+	do {
+		cout<<"\n\n*************** GESTION CUENTA JEFE ***************";
+		cout<<"\n1) Crear cuenta.";
+		cout<<"\n2) Modificar datos.";
+		cout<<"\n3) Salir.";
+		cout<<"\nDigite el numero de la opcion: "; cin>>opcion_cuenta;
+		
+		switch(opcion_cuenta){
+			case 1:{
+				cout<<"\n\n*************** CREAR CUENTA ***************";
+				cout<<"\nIngrese su id (5 dígitos): "; cin>>id;
+				cout<<"\nIngrese su contraseña (10 caracteres. Sin acento): ";
+				cin>>contrasenia;
+				// std::ios::app significa "Append" (Añadir). 
+				// Evita que el archivo se borre si ya existen otros jefes registrados.
+				ofstream archivo_jefes("archivo_jefes.txt",ios::app);
+				if(archivo_jefes.is_open()){
+				//escribimos los datos separados por comas. 
+				//también guarda los datos heredados de CPersona.
+				archivo_jefes <<id << ","<<contrasenia << "," <<nombre << "," <<apellido_paterno << "," <<edad <<"\n";
+				archivo_jefes.close();
+				cout<<"\n¡Cuenta registrada en archivo_jefes.txt!";
+				} else {cout<<"\n¡Error! No se pudo abrir o crear la base de datos.";}
+			} break;
+			
+			case 2:
+				cout<<"\n(El modulo de modificar datos esta en construccion...)";
+				// Aquí en el futuro leeremos el .txt, buscaremos el ID y reemplazaremos la línea.
+				break;
+				
+			case 3:
+				cout<<"\nRegresando al menu principal...";
+				break;
+			default:
+				cout<<"\nOpcion no valida.";
+		}
+		
+	}while (opcion_cuenta != 3);
 }
