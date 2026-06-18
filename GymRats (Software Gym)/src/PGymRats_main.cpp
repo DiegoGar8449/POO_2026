b35da0f667d0680d017b88d1f9cc57b6316c41c7
Diego Garcia <diegogarcia102010@hotmail.com>
2026-06-17 23:04:07 -0600

    Add files via upload
    Ahora este main.cpp únicamente tiene sus funciones y código correspondiente, para evitar código espaguetti.
    Se mejoró la lógica del flujo de datos para permitir salir al ingresar al perfil de jefe y para mejorar la naturalidad y apariencia de los mensajes.

--- GymRats (Software Gym)/src/PGymRats_main.cpp
+++ GymRats (Software Gym)/src/PGymRats_main.cpp
@@ -1,5 +1,5 @@
-/* Programa "GymRats". main.cpp del proyecto final.
- * Cabeceras utilizadas: CPersona.h, 
+/* Proyecto final: GymRats.
+ * PGymRats_main.cpp.
  * Autores:
  * - Contreras Galicia Jesús Yizán.
  * - García Rodríguez Diego.
@@ -8,7 +8,6 @@
  * Profa.: Dra. Claudia Azorín Vega.
  */
 #include <iostream>
-#include <fstream> //para manejo de archivos de texto y simular bd.
 #include "CPersona.h"
 using namespace std;
 
@@ -17,17 +16,19 @@ void limpiar_pantalla(); // limpia pantalla en linux y windows.
 
 /*************** 1. MAIN ***************/
 int main(){
-	cout<<"\n*************** GymRats (software para gestionar tu gym) ****************";
+	cout<<"\n*************** GYMRATS - Software para gestionar tu gym ****************";
 	
 	int continuar;
 	do{
 		limpiar_pantalla();
 		
 		int opcion;
-		cout<<"\n\n PERFILES GIMNASIO ***************";
-		cout<<"\n 1) Jefe. 2) Empleado. 3) Entrenador. 4) Cliente.";
-		cout<<"\nIngrese el numero de perfil al que quiera acceder: ";
+		cout<<"\n\n*************** PERFILES GIMNASIO ***************";
+		cout<<"\n1) Jefe." << "\n2) Empleado." << "\n3) Entrenador." << "\n4) Cliente.";
+		cout<<"\n****************************************";
+		cout<<"\n\nIngrese el numero del perfil que desee crear o escriba 0 para salir: ";
 		cin>>opcion;
+			
 		switch(opcion){
 			
 			case 1:
@@ -35,13 +36,16 @@ int main(){
 			jefe.solicitar_datos(); //mensaje
 			jefe.gestionar_cuenta();
 			break;
+			
+			
+			
 		} 
 		
-		cout<<"\n¿Seleccionar otro perfil? 1 = Si. 0 = No: ";
+		cout<<"\n¿Seleccionar otro perfil para crear? 1 = Si. 0 = No: ";
 		cin>> continuar;
 	} while (continuar==1);
 	
-	cout<<"\n¡Gracias por usar el programa!";
+	cout<<"\n¡Gracias por usar el programa!\n\n";
 	return 0;
 }
 
@@ -53,84 +57,4 @@ void limpiar_pantalla(){
 	#else
 			system("clear");//comando para linux.
 	#endif		
-}
-/*************** 2. FUNCIONES DEL PROGRAMADOR ***************/
-
-//CONSTRUCTOR CPersona
-CPersona::CPersona(){
-	nombre = "";
-	apellido_paterno = "";
-	apellido_materno = "";
-	edad = 0;
-	telefono_celular = "";
-	calle = "";
-	calle_num = "";
-	colonia = "";
-}
-//DESTRUCTOR CPersona
-CPersona::~CPersona(){}
-
-//DEFINICION METODOS CPersona
-void CPersona::solicitar_datos(){
-	cout<<"\n**************** INGRESE SUS DATOS ***************";
-	cout<<"\nNombre(s): ";
-	std::getline(std::cin >> std::ws, nombre);
-	cout<<"\nApellido paterno: "; cin>>apellido_paterno;
-	cout<<"\nApellido materno: "; cin>>apellido_materno;
-	cout<<"\nEdad: "; cin>>edad;
-	cout<<"\nTelefono celular: "; cin>>telefono_celular;
-	cout<<"\nCalle: "; cin>>calle;
-	cout<<"\nNumero de domicilio: "; cin>>calle_num;
-	cout<<"\nColonia: ";
-	std::getline(std::cin >> std::ws, colonia);
-	cout<<"****************************************";
-}
-
-//CONSTRUCTOR CJefe
-CJefe::CJefe() : CPersona(){
-	id = "";
-	contrasenia = "";
-}
-
-//DEFINICION METODOS CJefe
-void CJefe::gestionar_cuenta(){
-	int opcion_cuenta;
-	do {
-		cout<<"\n\n*************** GESTION CUENTA JEFE ***************";
-		cout<<"\n1) Crear cuenta.";
-		cout<<"\n2) Modificar datos.";
-		cout<<"\n3) Salir.";
-		cout<<"\nDigite el numero de la opcion: "; cin>>opcion_cuenta;
-		
-		switch(opcion_cuenta){
-			case 1:{
-				cout<<"\n\n*************** CREAR CUENTA ***************";
-				cout<<"\nIngrese su id (5 dígitos): "; cin>>id;
-				cout<<"\nIngrese su contraseña (10 caracteres. Sin acento): ";
-				cin>>contrasenia;
-				// std::ios::app significa "Append" (Añadir). 
-				// Evita que el archivo se borre si ya existen otros jefes registrados.
-				ofstream archivo_jefes("archivo_jefes.txt",ios::app);
-				if(archivo_jefes.is_open()){
-				//escribimos los datos separados por comas. 
-				//también guarda los datos heredados de CPersona.
-				archivo_jefes <<id << ","<<contrasenia << "," <<nombre << "," <<apellido_paterno << "," <<edad <<"\n";
-				archivo_jefes.close();
-				cout<<"\n¡Cuenta registrada en archivo_jefes.txt!";
-				} else {cout<<"\n¡Error! No se pudo abrir o crear la base de datos.";}
-			} break;
-			
-			case 2:
-				cout<<"\n(El modulo de modificar datos esta en construccion...)";
-				// Aquí en el futuro leeremos el .txt, buscaremos el ID y reemplazaremos la línea.
-				break;
-				
-			case 3:
-				cout<<"\nRegresando al menu principal...";
-				break;
-			default:
-				cout<<"\nOpcion no valida.";
-		}
-		
-	}while (opcion_cuenta != 3);
 }
