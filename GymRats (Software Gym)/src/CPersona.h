4faa7fcfd6f43f027bef8591fa758905d38a2c07
Diego Garcia <diegogarcia102010@hotmail.com>
2026-06-18 12:42:32 -0600

    Add files via upload
    La cabecera de la clase padre CPersona.h ahora únicamente tiene la declaración de sus propios atributos y métodos

--- GymRats (Software Gym)/src/CPersona.h
+++ GymRats (Software Gym)/src/CPersona.h
@@ -1,7 +1,7 @@
 #pragma once //para que la clase se compile una sola vez.
 #include <string>
 
-//***********Clase abstracta o Superclase****************
+/*********** Clase abstracta o Superclase ****************/
 class CPersona{
 	protected:
 	std::string nombre;//datos miembro
@@ -17,15 +17,4 @@ class CPersona{
 	void solicitar_datos();//funciones miembro
 	void mostrar_datos();
 	~CPersona();//destructor
-};
-
-class CJefe : public CPersona{
-	private:
-	std::string id;
-	std::string contrasenia;
-	public:
-	CJefe();
-	void gestionar_cuenta(); //Crear cuenta, Modificar datos personales o Eliminar cuenta.
-	void gestionar_empleado(); //Contratar empleado, Modificar o Despedir.
-	void pagar_empleado(); //pago quincenal a empleado e imprime ticket.
 };
